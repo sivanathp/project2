0a6100099060c6b1ec7cdae2d9a446c579cc350f
Sivanath Peddakotla <speddak1@my.westga.edu>
2021-06-05 11:45:47 -0400

    test scripts are compiling

--- src/app/app/services/roster-service.service.spec.ts
+++ src/app/app/services/roster-service.service.spec.ts
@@ -1,5 +1,4 @@
-import { TestBed } from '@angular/core/testing';
-import 'jasmine';
+import { TestBed, inject } from '@angular/core/testing';
 
 import { RosterServiceService } from './roster-service.service';
 
@@ -28,5 +27,49 @@ describe('RosterServiceService', () => {
     expect(rosterService.getContestants()[3]).toEqual('new one')
   });
   
-
+  it('should not allow duplicate names', () => {
+    expect(rosterService.getContestants().length).toEqual(3); 
+   let player:string = "new one";
+    expect(rosterService.addContestant(player)).toBeTruthy();    
+	
+	let newplayer:string = "new one";
+	expect(rosterService.addContestant(newplayer)).toBeFalsy();    
+   
+  });
+  
+  it('should not allow null names', () => {
+    expect(rosterService.getContestants().length).toEqual(3); 
+   
+	let newplayer:string = null;
+	expect(rosterService.addContestant(newplayer)).toBeFalsy();    
+   
+  });
+  
+  it('should not allow empty string names', () => {
+    expect(rosterService.getContestants().length).toEqual(3); 
+   let player:string = "";
+    expect(rosterService.addContestant(player)).toBeFalsy();    
+	 
+   
+  });
+  
+   it('should add one contestant', () => {
+    expect(rosterService.getContestants().length).toEqual(3); 
+   let player:string = "player 1";
+    expect(rosterService.addContestant(player)).toBeTruthy();    
+	 
+   
+  });
+  
+  it('should add several contestants', () => {
+    expect(rosterService.getContestants().length).toEqual(3); 
+   let player:string = "player 1";
+   let player2:string = "player 2";
+   let player3:string = "player 3";
+    expect(rosterService.addContestant(player)).toBeTruthy();    
+	expect(rosterService.addContestant(player2)).toBeTruthy();    
+	expect(rosterService.addContestant(player3)).toBeTruthy();    
+	 
+   
+  });
 });
