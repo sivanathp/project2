b74f81b425080d14f9bc410049be1760dbe3f0c5
Sivanath Peddakotla <speddak1@my.westga.edu>
2021-06-04 23:22:15 -0400

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HelloComponent } from './app/tournament/hello/hello.component';
+import { RegistrationComponent } from './app/tournament/registration/registration.component';
+import { BracketsComponent } from './app/tournament/brackets/brackets.component';
 
+const routes: Routes = [
+  { path: '', redirectTo: '/hello', pathMatch: 'full' },
+  { path: 'app/tournament/hello', component: HelloComponent },
+  { path: 'app/tournament/registration', component: RegistrationComponent },
+  { path: 'app/tournament/brackets', component: BracketsComponent },
+];
 
-const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelloComponent } from './app/tournament/hello/hello.component';
+import { RegistrationComponent } from './app/tournament/registration/registration.component';
+import { BracketsComponent } from './app/tournament/brackets/brackets.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent,
+    RegistrationComponent,
+    BracketsComponent
   ],
   imports: [
     BrowserModule,
