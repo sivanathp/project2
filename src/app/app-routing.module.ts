83c47a05afccd4f11d088179473a9c983eb6c5c7
speddak1 <speddak1@cs6261-vm07.uwg.westga.edu>
2021-06-05 11:27:44 -0400

    routing works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,10 @@ import { RegistrationComponent } from './app/tournament/registration/registratio
 import { BracketsComponent } from './app/tournament/brackets/brackets.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/hello', pathMatch: 'full' },
-  { path: 'app/tournament/hello', component: HelloComponent },
-  { path: 'app/tournament/registration', component: RegistrationComponent },
-  { path: 'app/tournament/brackets', component: BracketsComponent },
+ 
+  { path: 'hello', component: HelloComponent },
+  { path: 'registration', component: RegistrationComponent },
+  { path: 'brackets', component: BracketsComponent },
   { path: '**', redirectTo: '/hello' }  
 ];
 
